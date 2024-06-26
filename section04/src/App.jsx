88faf249161c341d4ddbc50555867ce0f7f3ad83
mhlee <karyo13234@gmail.com>
2024-06-26 14:40:44 +0900

    2.props

--- section04/src/App.jsx
+++ section04/src/App.jsx
@@ -2,13 +2,21 @@ import './App.css'
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Button from './components/Button';
 
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+  }
   return (
     <>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+      <Button {...buttonProps} ></Button >
+      <Button text={"카페"}></Button>
+      <Button text={"블로그"}>
+        <div>자식요소</div>
+        {/* <Header></Header> */}
+      </Button>
     </>
   )
 }
--- section04/src/components/Main.jsx
+++ section04/src/components/Main.jsx
@@ -1,14 +1,25 @@
-const Main = () => {
-  const number = 10;
-  const obj = { a: 1 };
-  return (
-    <>
-      <h1>main</h1>
-      <h2>{number % 2 === 0 ? "짝수" : "홀수"}</h2>
-      <h3>{obj.a}</h3>
+import "./Main.css";
 
-    </>
-  )
+const Main = () => {
+  const user = {
+    name: "이명한",
+    isLogin: true,
+  }
+
+  if (user.isLogin) {
+    return (
+      <div className='logout'>
+        로그아웃
+      </div>
+    );
+  } else {
+    return <div>로그인</div>;
+  }
+  // return (
+  //   <>
+  //     {user.isLogin ? (<div>로그아웃</div>) : (<div>로그인</div>)}
+  //   </>
+  // )
 }
 
 export default Main;
