d77a055227a60aa71d8b6138289d5252dd98270b
mhlee <karyo13234@gmail.com>
2024-06-26 15:17:33 +0900

    Revert "3.event-handler"
    This reverts commit 00a8e60983a2f08858c59c76548060b29ccc5c60.

--- section04/src/components/Button.jsx
+++ section04/src/components/Button.jsx
@@ -1,16 +1,6 @@
 const Button = ({ text, color, children }) => {
-  // 이벤트 객체
-  const onClickButton = (e) => {
-    console.log(e);
-    console.log(text);
-  }
-
   return (
-    <button
-      onClick={onClickButton}
-      // onMouseEnter={onClickButton}
-
-      style={{ color: color }}>
+    <button style={{ color: color }}>
       {text} -
       {color.toUpperCase()}
       {children}
