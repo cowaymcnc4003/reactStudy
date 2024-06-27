40c2ea4448a1165a7de944113498e05f8f740de4
mhlee <karyo13234@gmail.com>
2024-06-27 13:09:10 +0900

    7.react-custom-hook-useRef

--- section04/src/components/Register.jsx
+++ section04/src/components/Register.jsx
@@ -1,48 +1,63 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 // 간단한 회원가입 폼
 const Register = () => {
-  const [name, setName] = useState("이름");
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
+  const [input, setInput] = useState({ // 값이 변경되면 리렌더링
+    name: "",
+    birth: "",
+    country: "",
+    bio: ""
+  });
+  const countRef = useRef(0);
+  const inputRef = useRef();
+  console.log("countRef 랜더링");
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+
+  const onChange = (e) => {
+    countRef.current++;
+    console.log(countRef.current);
+    console.log(e.target.name, e.target.value);
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    })
   }
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
+
+  const onSubmit = () => {
+    if (input.name === "") {
+      inputRef.current.focus();
+    }
   }
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  }
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  }
+
   return (
     <div>
+      <button onClick={() => {
+        countRef.current++;
+        console.log(countRef.current);
+      }}>ref ++</button>
       <div>
-        <input value={name} onChange={onChangeName} placeholder={"이름"} type="text" />
-        {name}
+        <input ref={inputRef} value={input.name} name="name" onChange={onChange} placeholder={"이름"} type="text" />
+        {input.name}
       </div>
       <div>
-        <input value={birth} onChange={onChangeBirth} type="date" />
-        {birth}
+        <input value={input.birth} name="birth" onChange={onChange} type="date" />
+        {input.birth}
       </div>
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select value={input.country} name="country" onChange={onChange}>
           <option value=""></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
           <option value="uk">영국</option>
         </select>
-        {country}
+        {input.country}
       </div>
 
       <div>
-        <textarea value={bio} name="" id="" onChange={onChangeBio}></textarea>
-        {bio}
+        <textarea value={input.bio} name="bio" id="" onChange={onChange}></textarea>
+        {input.bio}
       </div>
+      <button onClick={onSubmit} >submit</button>
     </div>
   )
 }
